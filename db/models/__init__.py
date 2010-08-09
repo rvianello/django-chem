4ae5216b3ee18b13bd8320df61f18e54c048499c
ric <ric@3ae04688-2474-47ac-8984-c63c88e99703>
2010-08-09 15:05:22 +0000

    imported skel
    git-svn-id: http://svn.trentanove.com/code/trunk/django_chem@27 3ae04688-2474-47ac-8984-c63c88e99703

---
+++ db/models/__init__.py
@@ -0,0 +1,6 @@
+# Want to get everything from the 'normal' models package.
+from django.db.models import *
+
+# The chemistry-enabled fields.
+from django_chem.db.models.fields import MoleculeField
+
---
+++ db/models/fields.py
@@ -0,0 +1,4 @@
+from django.db.models.fields import Field
+
+class MoleculeField(Field):
+    pass
