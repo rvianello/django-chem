baf9b9851c950bc669ee3d3a3c8066ee561c3771
Riccardo Vianello <riccardo.vianello@gmail.com>
2011-02-10 22:09:35 +0100

    automatic injection of molecular weight field associated to molecule

--- django_chem/db/models/fields.py
+++ django_chem/db/models/fields.py
@@ -5,12 +5,29 @@ from django.db.models.sql.expressions import SQLEvaluator
 class ChemField(Field):
     pass
 
+class _MolecularWeight(FloatField):
+    "A molecular descriptor field corresponding to the molecular weight"
+
+    description = _('The molecular weight of the associated molecule')
+
+    def __init__(self, molecule, **kwargs):
+        """
+        The initialization function for AutoMolecularWeight fields. Takes the 
+        following special argument:
+
+        molecule:
+         the associated MoleculeField.
+        """
+        super(_MolecularWeight, self).__init__(**kwargs)
+
 class MoleculeField(ChemField):
     "The Molecule data type -- represents the chemical structure of a compound"
 
     description = _('Molecule (stored as a canonical-maybe SMILES string)')
 
-    def __init__(self, verbose_name=None, chem_index=False, **kwargs):
+    def __init__(self, verbose_name=None, chem_index=False, 
+                 auto_mw=False, auto_mw_name='mw', auto_mw_index=True,
+                 **kwargs):
         """
         The initialization function for Molecule fields.  Takes the following
         as keyword arguments:
@@ -23,11 +40,24 @@ class MoleculeField(ChemField):
         # Setting the index flag with the value of the `chem_index` keyword.
         self.chem_index = chem_index
 
+        # Information about automatically computed chemical descriptor fields
+        self.auto_mw = auto_mw
+        self.auto_mw_name = auto_mw_name
+        self.auto_mw_index = auto_mw_index
+
         # Setting the verbose_name keyword argument with the positional
         # first parameter, so this works like normal fields.
         kwargs['verbose_name'] = verbose_name
 
         super(MoleculeField, self).__init__(**kwargs)
+
+    def contribute_to_class(self, cls, name):
+        super(MoleculeField, self).contribute_to_class(cls, name)
+        if self.auto_mw:
+            self.mw_field = _MolecularWeight(self, db_index=self.auto_mw_index,
+                                             null=self.null, # null if molecule
+                                             )
+            self.mw_field.contribute_to_class(cls, self.auto_mw_name)
 
     def db_type(self, connection):
         return connection.ops.chem_db_type('MoleculeField')
@@ -81,7 +111,8 @@ class MoleculeField(ChemField):
             return value
 
         raise TypeError("Field has invalid lookup: %s" % lookup_type)
-    
+
+
 class FingerprintField(ChemField):
     pass
 
