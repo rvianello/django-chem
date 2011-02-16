97b827b1a10347c8b3382c5df2590640ae6b56ce
ric <ric@jabberwock.(none)>
2011-02-16 18:31:48 +0100

    minor adjustments.

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ extensions = [
     ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
+templates_path = []
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -123,7 +124,8 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
+html_static_path = []
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
--- docs/index.rst
+++ docs/index.rst
@@ -3,8 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to django-chem's documentation!
-=======================================
+django-chem
+===========
 
 Contents:
 
@@ -14,6 +14,7 @@ Contents:
    database_setup
    project_setup
    tutorial
+   api
 
 Indices and tables
 ==================
